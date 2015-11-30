c1d00317ecaa2a55ce4b467b8409b15b07d6f281
Edward Ruchevits <ruchevits@gmail.com>
2015-11-30 09:49:27 +0000

    Added common template helper for displaying user names

--- client/lib/helpers.js
+++ client/lib/helpers.js
@@ -3,3 +3,7 @@ Template.registerHelper('shortAddress', function(address) {
         return address.substring(0,12);
     }
 });
+
+Template.registerHelper('userName', function(name) {
+    return name ? name : 'Anonymous';
+});
